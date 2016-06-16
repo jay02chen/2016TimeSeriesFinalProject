fa650d3ea0fa229daf7cad6f092e949ed99855e9
jay02chen <r04921052@ntu.edu.tw>
2016-06-16 17:40:09 +0800

    update parameters in Q2,Q4

--- financial.R
+++ financial.R
@@ -1,13 +1,15 @@
 #!/usr/bin/env Rscript
 
 library("MTS")
-VARMAmodelSelection <- function(x,prd){
+VARMAmodelSelection <- function(x,diff){
 	#ACF of data
 	#tsx = ts(x)
 	#acfx = acf(tsx)
 	#acfx = acf(tsx,type="partial")
-
-	amodel = VARMA(x,0,0)
+	xx = x
+	for(times in 1:diff)
+		xx = xx[2:nrow(xx),] - xx[1:nrow(xx)-1,]
+	amodel = VARMA(xx,0,0)
 	aparam = c(0,0)
 	bmodel = amodel
 	bparam = aparam
@@ -17,13 +19,15 @@ VARMAmodelSelection <- function(x,prd){
 	for(p in 0:5){
 		for(q in 0:5){
 			result = tryCatch({
-				temp = VARMA(x,p,q)
+				temp = VARMA(xx,p,q)
 				if(temp$bic < bic){
+					print("update bic")
 					bmodel = temp
 					bic = bmodel$bic
 					bparam = c(p,q)
 				}
 				if(temp$aic < aic){
+					print("update aic")
 					amodel = temp
 					aic = amodel$aic
 					aparam = c(p,q)
@@ -46,7 +50,8 @@ VARMAmodelSelection <- function(x,prd){
 x = read.csv("data/4-2_financial.csv",header=F,sep=",")
 x = sapply(x[2:nrow(x), c(1,2)],as.character)
 class(x) <- "numeric"
-ret <- VARMAmodelSelection(x,4)
+
+ret <- VARMAmodelSelection(x,0)
 png(filename="financial_ACF_aic.png")
 acf(ret$aicModel$residual,lag=30)
 dev.off()
@@ -54,7 +59,7 @@ png(filename="financial_PACF_aic.png")
 acf(ret$aicModel$residual,type="partial",lag=30)
 dev.off()
 png(filename="financial_ACF_bic.png")
-acf(ret$bicModel$residual,lag=30)
+acf(ret$bicModel$residual,l	ag=30)
 dev.off()
 png(filename="financial_PACF_bic.png")
 acf(ret$bicModel$residual,type="partial",lag=30)
@@ -62,7 +67,7 @@ dev.off()
 
 print(ret$aicModel)
 print(BIC(ret$aicModel))
-print(ret$aicParam) #(5,5)
+print(ret$aicParam) #(5,5), d1(4,4), d2(5,5)
 print(ret$bicModel)
 print(BIC(ret$bicModel))
-print(ret$bicParam) #(2,0)
+print(ret$bicParam) #(2,0), d1(2,1), d2(0,4)
